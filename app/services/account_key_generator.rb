352645ac8a3e7d4a03d52d7de0d4e0ffb2a2a4c4
Leonardo Godoy <leonardo.godoy.souza@gmail.com>
2022-10-14 18:00:26 -0300

    Small styling improvements

--- app/services/account_key_generator.rb
+++ app/services/account_key_generator.rb
@@ -5,16 +5,16 @@ module AccountKeyGenerator
 
   def self.perform(email, key)
     form_data = {
-      "email": email,
-      "key": key
+      'email': email,
+      'key': key
     }
 
     response = HTTParty.post(ACCOUNT_KEY_URL, body: form_data.to_json, headers: { 'Content-Type': 'application/json' })
 
-    successful_request = response.success? && response["account_key"].present?
+    successful_request = response.success? && response['account_key'].present?
 
-    raise AccountKeyServiceError, response.parsed_response["error"] unless successful_request
+    raise AccountKeyServiceError, response.parsed_response['error'] unless successful_request
 
-    response.parsed_response["account_key"]
+    response.parsed_response['account_key']
   end
 end
